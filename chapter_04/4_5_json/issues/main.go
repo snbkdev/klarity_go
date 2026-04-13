204a78668240748d9a6ae27b57b89bc1f8ecaa35
Asanbek09 <samudinua@gmail.com>
2026-04-13 18:26:01 +0600

    4.5 JSON

---
+++ chapter_04/4_5_json/issues/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"program_lang/chapter_04/4_5_json/issues/github"
+)
+
+func main() {
+	result, err := github.SearchIssues(os.Args[1:])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%d тем:\n", result.TotalCount)
+	for _, item := range result.Items {
+		fmt.Printf("#%-5d %9.9s %.55s\n", item.Number, item.User.Login, item.Title)
+	}
+}
