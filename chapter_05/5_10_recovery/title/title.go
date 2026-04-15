e9974f5d190fb52649a97f9e5089b1ff7fd50eab
Asanbek09 <samudinua@gmail.com>
2026-04-15 13:01:14 +0600

    Завершение главы 5

---
+++ chapter_05/5_10_recovery/title/title.go
@@ -0,0 +1,52 @@
+package title
+
+import (
+	"fmt"
+
+	"golang.org/x/net/html"
+)
+
+func soleTitle(doc *html.Node) (title string, err error) {
+	type bailout struct{}
+
+	defer func() {
+		switch p := recover(); p {
+		case nil:
+			//
+		case bailout{}:
+			err = fmt.Errorf("несколько элементов title")
+		default:
+			panic(p)
+		}
+	}()
+
+	forEachNode(doc, func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "title" && n.FirstChild != nil {
+			if title != "" {
+				panic(bailout{})
+			}
+			title = n.FirstChild.Data
+		}
+	}, nil)
+
+	if title == ""{
+		return "", fmt.Errorf("нет элементов title")
+	}
+
+	return title, nil
+}
+
+
+func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
+	if pre != nil {
+		pre(n)
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+
+	if post != nil {
+		post(n)
+	}
+}
