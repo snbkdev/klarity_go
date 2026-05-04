a2bc356c30f43eda100ffcac519144a4a57691c4
Asanbek09 <samudinua@gmail.com>
2026-05-04 16:15:58 +0600

    Завершение главы 10

---
+++ chapter_10/10_5/example/main.go
@@ -0,0 +1,27 @@
+// 10.5 Пустой импорт
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/jpeg"
+	_ "image/png"
+	"io"
+	"os"
+)
+
+func main() {
+	if err := toJPEG(os.Stdin, os.Stdout); err != nil {
+		fmt.Fprintf(os.Stderr, "jpeg: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func toJPEG(in io.Reader, out io.Writer) error {
+	img, kind, err := image.Decode(in)
+	if err != nil {
+		return err
+	}
+	fmt.Fprintln(os.Stderr, "Входной формат = ", kind)
+	return jpeg.Encode(out, img, &jpeg.Options{Quality: 95})
+}
